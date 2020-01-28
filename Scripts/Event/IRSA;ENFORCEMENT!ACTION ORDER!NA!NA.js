be6a0f010bc8179ee39785a4211fca3b85baa303
AccelaAdmin <AccelaAdmin@accela.com>
2020-01-28 17:34:24 -0600

    Update IRSA;ENFORCEMENT!ACTION ORDER!NA!NA.js

--- Scripts/Event/IRSA;ENFORCEMENT!ACTION ORDER!NA!NA.js
+++ Scripts/Event/IRSA;ENFORCEMENT!ACTION ORDER!NA!NA.js
@@ -1,1 +1,10 @@
-//code here
+var recordType = lookup("ENF_INITIAL_INSP_MAPPING", inspResult);
+if (recordType)
+{
+	var newCap = createCap(recordType, "");
+	var linkResult = aa.cap.createAppHierarchy(capId, newCap);
+	if (linkResult.getSuccess())
+		logDebug("Successfully linked to Parent Application");
+	else
+		logDebug( "**ERROR: linking to parent application parent cap " + linkResult.getErrorMessage());	
+}
