45a2795a7fcf0898d832ae21cecd4b4681e87dc4
AccelaAdmin <AccelaAdmin@accela.com>
2020-01-30 14:31:18 -0600

    Create IRSA;ENFORCEMENT!~!~!~.js

---
+++ Scripts/Event/IRSA;ENFORCEMENT!~!~!~.js
@@ -0,0 +1,16 @@
+if (matches(inspResult, "Pass", "Fail"))
+{
+	var gsItems = getGuideSheetObjects(inspId);
+	var vTable = new Array();
+	for (g in gsItems)
+	{
+		var thisItem = gsItems[g];
+		var row = new Array();
+		row["Violation"] = thisItem.text;
+		row["Status"] = thisItem.status;
+		row["Inspector Comment"] = thisItem.comment;
+		vTable.push(row);
+	}
+	removeASITable("VIOLATIONS");
+	addASITable("VIOLATIONS", vTable);
+}
