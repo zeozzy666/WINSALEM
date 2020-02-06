2611b788734ba3a2049607cead13f459cc363a1a
AccelaAdmin <AccelaAdmin@accela.com>
2020-02-06 08:06:36 -0600

    Update WTUA;ENFORCEMENT!HOUSING!~!~.js

--- Scripts/Event/WTUA;ENFORCEMENT!HOUSING!~!~.js
+++ Scripts/Event/WTUA;ENFORCEMENT!HOUSING!~!~.js
@@ -1,4 +1,4 @@
-if ("Follow-Up Investagation".equals(wfTask) && "Not in Compliance - New Violation".equals(wfStatus))
+if ("Follow-Up Investagation".equals(wfTask) && "Not In Compliance - New Violation".equals(wfStatus))
 {
 	deactivateTask("Follow-Up Investagation");
 	activateTask("Legal Hearing");
