a2d3bc33f7760820b844ecf04dc7c70bbafca00a
AccelaAdmin <accelaadmin@accela.com>
2020-03-06 11:14:36 -0600

    Update ASA;ENFORCEMENT!~!~!~.js

--- Scripts/Event/ASA;ENFORCEMENT!~!~!~.js
+++ Scripts/Event/ASA;ENFORCEMENT!~!~!~.js
@@ -1,8 +1,8 @@
 if (!matches(appTypeArray[1], "Action Order"))
 {
-	scheduleInspection("Initial Inspection", 14);
+	scheduleInspection("Initial Inspection", 14, currentUserID);
 }
 else
 {
-	scheduleInspection("Initial Investigation", 1, currentUserID);
+	scheduleInspection("Initial Investigation", 1);
 }
