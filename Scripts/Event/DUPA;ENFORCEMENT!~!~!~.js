ec4cc62443e721af1c10652c3b48e96746ab3c54
AccelaAdmin <AccelaAdmin@accela.com>
2020-04-07 09:01:06 -0500

    Update DUPA;ENFORCEMENT!~!~!~.js

--- Scripts/Event/DUPA;ENFORCEMENT!~!~!~.js
+++ Scripts/Event/DUPA;ENFORCEMENT!~!~!~.js
@@ -1,1 +1,10 @@
-refreshDocTracking(documentModel.documentNo);
+var requestPOD = getDocCustomField(documentModel.documentNo, "Request POD");
+if ("CHECKED".equals(requestPOD))
+{
+	refreshDocTracking(documentModel.documentNo, true);
+	setDocCustomField(documentModel.documentNo, "Request POD", "UNCHECKED");
+}
+else
+{
+	refreshDocTracking(documentModel.documentNo);
+}
