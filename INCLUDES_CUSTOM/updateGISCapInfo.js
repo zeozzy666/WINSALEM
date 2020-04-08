1925c8705593e75a95d53cafdc4583514ab46803
AccelaAdmin <AccelaAdmin@accela.com>
2020-04-08 12:43:38 -0500

    Update updateGISCapInfo.js

--- INCLUDES_CUSTOM/updateGISCapInfo.js
+++ INCLUDES_CUSTOM/updateGISCapInfo.js
@@ -4,7 +4,7 @@ function updateGISCapInfo()
 	if (arguments.length > 0)
 		itemCap = arguments[0];
 	var pin = getGISInfo("WINSALEM", "Parcels", "PIN") || "";
-	var ward = getGISInfo("WINSALEM", "GISADMIN.Code_Enforcement_Territories", "Territory"); || "";
+	var ward = getGISInfo("WINSALEM", "GISADMIN.Code_Enforcement_Territories", "Territory") || "";
 	var inspector = getGISInfo("WINSALEM", "GISADMIN.Code_Enforcement_Territories", "NCO") || "";
 
 	editAppSpecific("Ward", ward, itemCap);
