cdb1ec973809cdcc15222c5d9882a3a21ef2efc6
AccelaAdmin <AccelaAdmin@accela.com>
2020-04-13 13:49:20 -0500

    Update ASA;ENFORCEMENT!~!~!~.js

--- Scripts/Event/ASA;ENFORCEMENT!~!~!~.js
+++ Scripts/Event/ASA;ENFORCEMENT!~!~!~.js
@@ -5,6 +5,11 @@ if (!matches(appTypeArray[1], "Action Order"))
 else
 {
 	scheduleInspection("Initial Investigation", 1);
+	//assign new cap
+	var gisInspector = getGISInfo("WINSALEM", "GISADMIN.Code_Enforcement_Territories", "NCO");
+	var accelaInspector = lookup("WINSALEM_SETTINGS_GIS_INSPECTORS", gisInspector);
+	if (accelaInspector)
+		assignCap(accelaInspector);	
 }
 //update GIS info
 updateGISCapInfo();
