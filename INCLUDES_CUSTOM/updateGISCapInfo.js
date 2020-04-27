86e4bf95e389aca71f3a13deb0c2ef8fd7350800
AccelaAdmin <AccelaAdmin@accela.com>
2020-04-27 15:36:31 -0500

    added zoning to gis info

--- INCLUDES_CUSTOM/updateGISCapInfo.js
+++ INCLUDES_CUSTOM/updateGISCapInfo.js
@@ -6,8 +6,10 @@ function updateGISCapInfo()
 	var pin = getGISInfo("WINSALEM", "Parcels", "PIN") || "";
 	var ward = getGISInfo("WINSALEM", "GISADMIN.Code_Enforcement_Territories", "Territory") || "";
 	var inspector = getGISInfo("WINSALEM", "GISADMIN.Code_Enforcement_Territories", "NCO") || "";
+	var zoning = getGISInfo("WINSALEM", "Zoning", "Zoning_District") || "";
 
 	editAppSpecific("Ward", ward, itemCap);
 	editAppSpecific("PIN", pin, itemCap);
 	editAppSpecific("Inspector", inspector, itemCap);
+	editAppSpecific("Zoning", zoning, itemCap);
 }
