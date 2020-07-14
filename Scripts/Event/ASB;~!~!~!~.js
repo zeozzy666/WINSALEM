d4f92b6e5172e4809dd0b03368d7379f80f61971
AccelaAdmin <AccelaAdmin@accela.com>
2020-07-14 13:09:11 -0500

    commenting out

--- Scripts/Event/ASB;~!~!~!~.js
+++ Scripts/Event/ASB;~!~!~!~.js
@@ -1,7 +1,8 @@
-var appType = aa.env.getValue("ApplicationTypeLevel2");
+/*var appType = aa.env.getValue("ApplicationTypeLevel2");
 if (!"Action Order".equals(appType))
 {
     cancel = true;
     showMessage = true;
     comment("Cases must start with an Action Order")
 }
+*/
