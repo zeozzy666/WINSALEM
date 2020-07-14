52aa2087c3d66f4880fefbb124b3cbed9f89ab24
AccelaAdmin <AccelaAdmin@accela.com>
2020-07-14 12:14:27 -0500

    Create ASB;~!~!~!~.js

---
+++ Scripts/Event/ASB;~!~!~!~.js
@@ -0,0 +1,7 @@
+var appType = aa.env.getValue("ApplicationTypeLevel2");
+if (!"Action Order".equals(appType))
+{
+    cancel = true;
+    showMessage = true;
+    comment("Cases must start with an Action Order")
+}
