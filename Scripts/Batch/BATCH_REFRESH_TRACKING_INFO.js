a2dc561de7969e35f93c80c8406ca0268372edeb
Mark Wells <70722038+mwells-Accela@users.noreply.github.com>
2021-01-22 13:38:04 -0800

    Update to newest version

--- Scripts/Batch/BATCH_REFRESH_TRACKING_INFO.js
+++ Scripts/Batch/BATCH_REFRESH_TRACKING_INFO.js
@@ -46,9 +46,9 @@ logMessage("END", "End of Job: Elapsed Time : " + elapsed() + " Seconds");
 //#region Main
 function mainProcess() {
     var capIDListOutput,
-    capIDList = new Array(),
-    capTypeModel,
-    capModel;
+        capIDList = new Array(),
+        capTypeModel,
+        capModel;
     var appTypeArray = appType.split("/");
     var status2SkipArray = status2Skip.split(",");
 
@@ -63,13 +63,11 @@ function mainProcess() {
     capModel.setCapType(capTypeModel);
     //capModel.setCapStatus(appStatus);    
     capIDListOutput = aa.cap.getCapIDListByCapModel(capModel).getOutput();
-    for (var c in capIDListOutput)
-    {
+    for (var c in capIDListOutput) {
         capId = aa.cap.getCapID(capIDListOutput[c].getID1(), capIDListOutput[c].getID2(), capIDListOutput[c].getID3()).getOutput();
         altId = capId.getCustomID();
         var capStatus = getAppStatus(capId) || "";
-        if (exists(capStatus, status2SkipArray) || capStatus.indexOf("Close") >= 0)
-            { logDebug(altId + ": " + capStatus + " is a skip status, skipping..."); continue; }
+        if (exists(capStatus, status2SkipArray) || capStatus.indexOf("Close") >= 0) { logDebug(altId + ": " + capStatus + " is a skip status, skipping..."); continue; }
         //refresh tracking
         refreshDocTrackingASIT(capId);
 
@@ -84,17 +82,16 @@ function elapsed() {
     var thisTime = thisDate.getTime();
     return ((thisTime - startTime) / 1000)
 }
- function getRecords()
- {
-    var sql = "";
+function getRecords() {
+    var sql = "SELECT DISTINCT B1_ALT_ID FROM dbo.B1PERMIT B INNER JOIN dbo.BAPPSPECTABLE_VALUE ASIT ON ASIT.SERV_PROV_CODE = B.SERV_PROV_CODE AND ASIT.B1_PER_ID1 = B.B1_PER_ID1 AND ASIT.B1_PER_ID2 = B.B1_PER_ID2 AND ASIT.B1_PER_ID3 = B.B1_PER_ID3 WHERE ASIT.TABLE_NAME = 'HOURS' AND B.SERV_PROV_CODE = 'WINSALEM'";
     var array = new Array();
     try {
         var initialContext = aa.proxyInvoker.newInstance("javax.naming.InitialContext", null).getOutput();
-        var ds = initialContext.lookup("java:/AA");
+        var ds = initialContext.lookup("java:/WINSALEM");
         var conn = ds.getConnection();
-        var sStmt = conn.prepareStatement(sql);
+        var sStmt = aa.db.prepareStatement(conn, sql);
         var rSet = sStmt.executeQuery();
-       while (rSet.next()) {
+        while (rSet.next()) {
             var obj = {};
             var md = rSet.getMetaData();
             var columns = md.getColumnCount();
@@ -108,17 +105,16 @@ function elapsed() {
 
     } catch (err) {
         aa.print(err.message);
-    }    
-    finally
-    {   
+    }
+    finally {
         if (rSet)
             rSet.close();
         if (sStmt)
             sStmt.close();
         if (conn)
-            conn.close();        
+            conn.close();
     }
- }
+}
 function getScriptText(vScriptName, servProvCode, useProductScripts) {
     if (!servProvCode)
         servProvCode = aa.getServiceProviderCode();
@@ -136,3 +132,4 @@ function getScriptText(vScriptName, servProvCode, useProductScripts) {
     }
 }
 //#endregion
+
