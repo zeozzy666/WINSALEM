82a8151de6b5b22cec2e33e012efbd66c8cd786c
Mark Wells <70722038+mwells-Accela@users.noreply.github.com>
2021-02-04 12:23:59 -0800

    test

--- Scripts/Integrations/GETINVOICEDFEEITEMS.js
+++ Scripts/Integrations/GETINVOICEDFEEITEMS.js
@@ -39,10 +39,10 @@ try
         thisFeeItem.totalCost = thisRecord["GF_FEE"] ? thisRecord["GF_FEE"] : "";
         thisFeeItem.feeSeq = thisRecord["FEEITEM_SEQ_NBR"] ? thisRecord["FEEITEM_SEQ_NBR"] : "";
         thisFeeItem.caseCloseDate  = getCloseDate();
-        thisFeeItem.Area = new String(getGISInfo("WINSALEM", "Wards", "Ward")) || "";
-        thisFeeItem.Pin = new String(getGISInfo("WINSALEM", "Parcels", "PIN")) || "";
-        thisFeeItem.Block = new String(getGISInfo("WINSALEM", "Parcels", "BLK")) || "";
-        thisFeeItem.Lot = new String(getGISInfo("WINSALEM", "Parcels", "LOT")) || "";
+        //thisFeeItem.Area = new String(getGISInfo("WINSALEM", "Wards", "Ward")) || "";
+        //thisFeeItem.Pin = new String(getGISInfo("WINSALEM", "Parcels", "PIN")) || "";
+        //thisFeeItem.Block = new String(getGISInfo("WINSALEM", "Parcels", "BLK")) || "";
+        //thisFeeItem.Lot = new String(getGISInfo("WINSALEM", "Parcels", "LOT")) || "";
         thisFeeItem.location = "";
         thisFeeItem.chargeType = "";
         thisFeeItem.chargeCode = thisRecord["GF_L1"] ? thisRecord["GF_L1"] : "";
@@ -60,9 +60,9 @@ finally
 {
 	result.message = message;
     result.invoicedFeeItemsArray = invoicedFeeItemsArray;
-    aa.env.setValue("returnCode", "1");
-    aa.env.setValue("returnValue", JSON.stringify(result));
-    //aa.env.setValue("result", result);
+    //aa.env.setValue("returnCode", "1");
+    //aa.env.setValue("returnValue", JSON.stringify(result));
+    aa.env.setValue("result", result);
 }
 
 function getRecords(fDate, tDate)
